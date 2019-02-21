0bece88a8e4157bcf40229e7a303dfd412472314
jaskaran <jaskaran.sarkaria@googlemail.com>
2019-02-21 13:55:25 +0000

    loop to return all matches

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const PORT = 3100;
 const fs = require("fs");
+const url = require("url");
 const app = express();
 
 app.get("/", (req, res) => {});
 
 app.get("/name", (req, res) => {
-  let query = req.query;
-  console.log({ query });
   let bollywoodName = req.query.name;
+  console.log(bollywoodName);
   fs.readFile("./bollywood.json", "utf8", (error, bollywoodData) => {
     if (error) {
       console.error(error);
+      return res.json({ error: error });
     }
-    let data = JSON.parse(bollywoodData);
+    let data = JSON.parse(bollywoodData); // turn it into JS
     if (!bollywoodName) {
+      // if no query
       res.json(data);
     }
     let name = data[bollywoodName];
@@ -25,7 +27,26 @@ app.get("/name", (req, res) => {
 app.get("/gender/:bollywoodGender", (req, res) => {
   let { bollywoodGender } = req.params;
   console.log("gender", bollywoodGender);
-  res.json({});
+  fs.readFile("./bollywood.json", "utf8", (error, bollywoodData) => {
+    if (error) {
+      console.error(error);
+      return res.json({ error: error });
+    }
+    let data = JSON.parse(bollywoodData); // turn it into JS
+    if (!bollywoodGender) {
+      // if no query
+      res.json(data);
+    }
+    console.log(data);
+    let matches = {};
+    for (let person in data) {
+      console.log(bollywoodGender);
+      if (data[person].gender === bollywoodGender) {
+        matches[person] = data[person];
+      }
+    }
+    res.json(matches);
+  });
 });
 
 app.listen(PORT, () => {
