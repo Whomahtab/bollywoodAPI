c86fb7546ae6bd321c126e4510090755860777ec
ropenfold <stuartroper@gmail.com>
2019-02-21 14:04:40 +0000

    changes to queries

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const PORT = 3100;
 const fs = require("fs");
+const express = require("express");
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  req.json({ status: "API is working" });
+  res.json({ status: "API is working" });
 });
 
 app.get("/name", (req, res) => {
@@ -33,6 +34,23 @@ app.get("/gender/:bollywoodGender", (req, res) => {
   let { bollywoodGender } = req.params;
   console.log("gender", bollywoodGender);
   res.json({});
+});
+
+app.get("/age", (req, res) => {
+  let query = req.query;
+  console.log({ query });
+  let ageQuery = req.query.age;
+  fs.readFile("./bollywood.json", "utf8", (error, bollywoodData) => {
+    if (error) {
+      console.error(error);
+    }
+    let data = JSON.parse(bollywoodData);
+    if (!ageQuery) {
+      res.json(data);
+    }
+    let age = data[ageQuery];
+    res.json({ age: age });
+  });
 });
 
 app.listen(PORT, () => {
